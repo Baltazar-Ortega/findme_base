865f0395db7a05d9b0dbd7dcbbc2591e270411e6
Baltazar-Ortega} <kalbertog121@gmail.com>
2019-05-21 09:59:50 -0500

    validacion registro login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,21 +10,48 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  email: string;
-  password: string;
+  email: string = null;
+  password: string = null;
 
-  constructor(private authService: AuthService, public router: Router) { }
+  constructor(private authService: AuthService, public router: Router, public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
   onSubmitLogin() {
+    if (this.email === '' || this.password === '') {
+      console.log('Llena los datos');
+      return;
+    }
     this.authService.login(this.email, this.password).then(res => {
       // Redirigir a tab1
       this.router.navigate(['/dashboard-perdido']);
     }).catch(err => {
-      alert('Los datos son incorrectos o no existe el usuario');
-    })
+      this.presentarAlert(err);
+      // alert('Los datos son incorrectos o no existe el usuario');
+    });
+  }
+
+  async presentarAlert(err) {
+    let message = 'Datos incorrectos';
+    console.log(err);
+    if (!this.password) {
+      message = 'Ingresa una contraseña';
+   }
+    if (!this.email) {
+     message = 'Ingresa un email';
+   }
+    if(err.code === 'auth/wrong-password') {
+      message = 'Contraseña incorrecta';
+    } else if (err.code === 'auth/user-not-found') {
+      message = 'No existe un usuario con ese email';
+    }
+    const alert = await this.alertCtrl.create({
+      header: 'Algo salió mal...',
+      message,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,3 +1,4 @@
+import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  public email: string;
-  public password: string;
-  public nombreUsuario: string;
-  public telefono: string;
+  public email: string = null;
+  public password: string = null;
+  public nombreUsuario: string = null;
+  public telefono: string = null;
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router, private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
@@ -25,7 +26,36 @@ export class RegistroPage implements OnInit {
       console.log(auth); // Aqui se pueden manejar las validaciones
     }).catch(err => {
       console.log(err);
+      this.presentAlert(err);
     });
+  }
+
+  async presentAlert(err) {
+    let message = 'Ingresa otros datos';
+    console.log(this.password);
+    if (!this.password) {
+       message = 'Establece una contraseña';
+    }
+    if (!this.email) {
+      message = 'Establece un email';
+    }
+    if (!this.telefono) {
+      message = 'Establece un telefono';
+    }
+    if (!this.nombreUsuario) {
+      message = 'Establece un nombre de usuario';
+    }
+    if (err.code === 'auth/email-already-in-use') {
+      message = 'El email introducido ya está en uso';
+    } else if (err.code === 'auth/invalid-email') {
+      message = 'El formato del email es incorrecto';
+    }
+    const alert = await this.alertCtrl.create({
+      header: 'Algo salió mal...',
+      message,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
