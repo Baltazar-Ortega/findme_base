fe7cc8b09905dc780c6103f66e35d667db631095
Baltazar-Ortega} <kalbertog121@gmail.com>
2019-05-22 01:12:39 -0500

    meses con nombre

--- src/app/perdidos/detalle-perdido/detalle-perdido.component.ts
+++ src/app/perdidos/detalle-perdido/detalle-perdido.component.ts
@@ -51,6 +51,20 @@ export class DetallePerdidoComponent implements OnInit {
     if (day.length < 2) {
       day = '0' + day;
     }
+    switch (month) {
+      case '01': month = 'Enero'; break;
+      case '02': month = 'Febrero'; break;
+      case '03': month = 'Marzo' ; break;
+      case '04': month = 'Abril'; break;
+      case '05': month = 'Mayo'; break;
+      case '06': month = 'Junio'; break;
+      case '07': month = 'Julio'; break;
+      case '08': month = 'Agosto'; break;
+      case '09': month = 'Septiembre'; break;
+      case '10': month = 'Octubre'; break;
+      case '11': month = 'Noviembre'; break;
+      case '12': month = 'Diciembre'; break;
+    }
     return `${day}-${month}-${year}`;
   }
 
